a60fd4b4134bbaf86372e069da8b477d3bae7553
SindhuraVegi <39994414+SindhuraVegi@users.noreply.github.com>
2018-12-08 00:49:03 +0400

    Update Trigger_after_insert_evaluation.sql

--- model/Trigger_after_insert_evaluation.sql
+++ model/Trigger_after_insert_evaluation.sql
@@ -1,28 +1,27 @@
 DELIMITER $$
-DROP TRIGGER IF EXISTS after_insert_evaluation_trg$$
-
-CREATE TRIGGER after_insert_evaluation_trg
+DROP TRIGGER evaluation_after_insert_trg$$
+CREATE TRIGGER evaluation_after_insert_trg
 AFTER INSERT ON evaluation
 FOR EACH ROW
 BEGIN
-	DECLARE v_user_id int;
-    DECLARE finished BOOLEAN DEFAULT FALSE;
-	DECLARE c_rows CURSOR FOR
+	DECLARE finished BOOLEAN;
+    DECLARE v_user_id int; 
+	DECLARE c_users CURSOR FOR 
     SELECT user_id 
-    FROM group_member
-	WHERE group_id = NEW.group_id;
-	OPEN c_rows;
-	BEGIN
-		-- Exit this BEGIN block when no more row
-	 DECLARE EXIT HANDLER FOR NOT FOUND SET finished = TRUE;
-	 REPEAT
-	 FETCH c_rows INTO v_user_id;
-	 -- Next line not executed if no data found
-	 -- as EXIT HANDLER when NOT FOUND
-	 INSERT INTO sheet(trainee_id, evaluation_id)
-	 VALUES(v_user_id, NEW.evaluation_id);
-	 UNTIL finished END REPEAT;
-	END;
-	CLOSE c_rows;
+	FROM group_member
+	WHERE group_id = new.evaluation_id;
 
+	-- INSERT the sheets
+    SET finished = FALSE;
+    OPEN c_users;
+	BEGIN
+		 -- Exit this BEGIN block when no more row
+		 DECLARE EXIT HANDLER FOR NOT FOUND SET finished = TRUE;
+		 REPEAT
+		 FETCH c_users INTO v_user_id;
+		 INSERT INTO sheet(trainee_id, evaluation_id, started_at) 
+         VALUES (v_user_id, new.evaluation_id, NULL);
+		UNTIL finished END REPEAT;
+	 END;
+	 CLOSE c_users;
 END $$
