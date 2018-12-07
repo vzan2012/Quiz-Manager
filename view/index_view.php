08f6b0792a71913b839eacd9799386378bbbd2fc
Deepak Guptha <vzan2012@gmail.com>
2018-12-08 02:38:20 +0530

    Minor Modify in index_view.php file

--- view/index_view.php
+++ view/index_view.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html>
-   <head>
-      <link rel="stylesheet" type="text/css" href="css/example.css"/>
-      <title>Quiz Manager Index Page</title>
-   </head>
-   <body>
-      <?php
+<head>
+   <link rel="stylesheet" type="text/css" href="css/example.css"/>
+   <title>Quiz Manager Index Page</title>
+
+   <!-- Bootstrap Styles -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+
+   <!-- Custom Styles -->
+
+   
+</head>
+<body>
+   <?php
       // Header shared by all pages
-      require_once("header.php");
-      ?>
-      <h1>Quiz Manager</h1>
-      <p>
-		This is an application developed in PHP, with form and session management.
-      </p>
-	
-	</body>
+   require_once("header.php");
+   ?>
+   <h1>Quiz Manager</h1>
+   <p>
+    This is an application developed in PHP, with form and session management.
+ </p>
+
+</body>
+</html>
