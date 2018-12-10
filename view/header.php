e39cd899b43907d5520175ccbf3b13674fd6dbf7
Deepak Guptha <vzan2012@gmail.com>
2018-12-10 19:03:31 +0530

    Updated the Bootstrap styles

--- view/header.php
+++ view/header.php
@@ -11,15 +11,30 @@ function display_error($key, $tag = "span") {
    }
 }
 ?>
-<nav>
-   <a href="./">Home</a>
-   <a href="login">Login</a>
-   <a href="../doc/">Documentation</a>
+<nav class="navbar navbar-inverse">
+   <div class="container-fluid">
+      <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand hide" href="#">Brand</a>
+      </div>
+   </div>
+   
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">   
+         <li><a href="./">Home</a></li>
+         <li><a href="login">Login</a></li>
+         <li><a href="../doc/">Documentation</a></li>
+      </ul>
    <?php
    if (isset($_SESSION["user_sql_skills"])) {
       // Connected => may disconnect
       ?>
-      <form action="sign_out" method="POST" id="loginForm">
+      <form class="navbar-form navbar-right" action="sign_out" method="POST" id="loginForm">
          <button type="submit">Disconnect
             <?= $_SESSION["user_sql_skills"]["email"] ?></button>
       </form>
@@ -28,10 +43,10 @@ function display_error($key, $tag = "span") {
       $login = filter_input(INPUT_POST, "login");
       // Not connected => login form, with the potential error message
       ?>
-      <form action="sign_in" method="POST" id="loginForm">
-         <input type="text" name="login" value="<?= $login ?>" placeholder="Your email" title="Your email"/>
-         <input type = "password" name = "password" placeholder="Your password" title="Your password"/>
-         <button type="submit">Connect</button>
+      <form class="navbar-form navbar-right" action="sign_in" method="POST" id="loginForm">
+         <input class="form-control" type="text" name="login" value="<?= $login ?>" placeholder="Your email" title="Your email"/>
+         <input class="form-control" type = "password" name = "password" placeholder="Your password" title="Your password"/>
+         <button class="btn btn-primary"type="submit">Connect</button>
          <?= display_error("login_form", "div") ?>
       </form>
       <?php
--- view/index_view.php
+++ view/index_view.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+   
    <link rel="stylesheet" type="text/css" href="css/example.css"/>
    <title>Quiz Manager Index Page</title>
+
 
    <!-- Bootstrap Styles -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 
    <!-- Custom Styles -->
+   <link rel="stylesheet" href="../pages/static/css/custom-styles.css">
 
-   
+
 </head>
 <body>
    <?php
@@ -18,8 +21,14 @@
    ?>
    <h1>Quiz Manager</h1>
    <p>
-    This is an application developed in PHP, with form and session management.
- </p>
+     This is an application developed in PHP, with form and session management.
+   </p>
+
+   <!-- Bootstrap Scripts -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+   <!-- Custom Scripts -->
+   <script src="../pages/static/js/custom-scripts.js"></script>
 
 </body>
 </html>
