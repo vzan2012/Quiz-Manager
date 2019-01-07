47c088cbf4a3b4fbb2b9c907638a426114ecebed
Deepak Guptha <vzan2012@gmail.com>
2019-01-07 15:03:08 +0530

    Working on the Sheet file.

---
+++ model/sheet_model.php
@@ -0,0 +1,37 @@
+<?php
+
+// Include class DB found in DB.php
+require_once("DB.php");
+
+class Sheet {
+
+   /** Get the evaluation of id $id as an associative array,
+    *  or null if not found */
+   public static function update($traineeId, $evalId) {
+      // Get a db connection
+      $db = DB::getConnection();
+      // Parameterized SQL query
+      $sql = "
+  		SELECT evaluation_id,title,diagram_path,scheduled_at,ending_at
+  		FROM 
+  			evaluation e
+  				INNER JOIN
+  			sql_quiz sq ON e.quiz_id = sq.quiz_id
+              INNER JOIN
+        quiz_db qd ON sq.db_name = qd.db_name
+  		WHERE e.evaluation_id = :id";
+      // Compile the request
+      $stmt = $db->prepare($sql);
+      // Set the parameter
+      $stmt->bindValue(":id", $id);
+      // Execute the request
+      $stmt->execute();
+      // Return the row as an associative array, or null if not found
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+   }
+
+   public static function getQuestions($quizId) {
+
+   }
+
+}
