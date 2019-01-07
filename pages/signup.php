2e823633dfb43884e5820d733fa13a27fc133208
shantanu04 <kambleshan04@gmail.com>
2019-01-08 00:08:41 +0100

    Added signup view and controller

---
+++ pages/signup.php
@@ -0,0 +1,73 @@
+<?php
+/** Controller for sign up
+ */
+
+
+// define variables and set to empty values
+$lastNameErr = $firstNameErr = $emailErr = $passwordErr = "";
+$lastName = $firstName = $email = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // validate lastname
+  if (empty($_POST["lastname"])) {
+    $lastNameErr = "Lastname is required";
+  } else {
+    $lastName = test_input($_POST["lastname"]);
+    // check if lastname only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$lastName)) {
+      $lastNameErr = "Only letters and white space allowed"; 
+    }
+  }
+  // validate firstname
+  if (empty($_POST["firstname"])) {
+    $firstNameErr = "Firstname is required";
+  } else {
+    $firstName = test_input($_POST["firstname"]);
+    // check if firstname only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$firstName)) {
+      $firstNameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  // validate email
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+
+  // validate password
+  if (empty($_POST["password"])) {
+    $passwordErr = "Password is required";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+// Get id parameter
+/*$evaluation_id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+if ($evaluation_id === null // no value
+  || $evaluation_id == false) { // not an integer
+  $errors["id"] = "id parameter must be set and integer (eg: evaluation-1)";
+}
+else {
+  // Call the model
+  require_once("../model/evaluation_model.php");
+
+  $evaluation = Evaluation::get($evaluation_id);
+}
+*/
+
+// Send to the view
+require_once("../view/signup_view.php");
---
+++ view/signup_view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<h2>User Sign Up</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="signup">  
+  FirstName: <input type="text" name="firstname" value="<?php echo $firstName;?>">
+  <span class="error">* <?php echo $firstNameErr;?></span>
+  <br><br>
+  LastName: <input type="text" name="lastname" value="<?php echo $lastName;?>">
+  <span class="error">* <?php echo $lastNameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Password: <input type="password" name="password" value="<?php echo $password;?>">
+  <span class="error">*<?php echo $passwordErr;?></span>
+  <br><br>
+
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+</body>
+</html>
